e237c9dac043a490648fb33a12636c85f8b424ca
sachaamm <sachaamm@gmail.com>
2021-01-04 16:07:05 +0100

    set frame rate

---
+++ Assets/Scripts/Player/SetTargetFrameRate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class SetTargetFrameRate : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Application.targetFrameRate = 300;
+        }
+    }
+}
