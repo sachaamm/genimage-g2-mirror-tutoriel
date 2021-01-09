c347c81d120ed105a0d18e5206e4dedc49bc83d8
sachaamm <sachaamm@gmail.com>
2021-01-09 19:53:23 +0100

    Cars

---
+++ Assets/Scripts/Player/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+namespace Player
+{
+    public class Car : NetworkBehaviour
+    {
+        [SyncVar] private int positionClassement = 1;
+
+        [SyncVar] private int tour = 1;
+
+        [SyncVar] private int indexCheckPoint = 1;
+        
+        void Start()
+        {
+            CarUtility.Singleton.AddCar(this);
+        }
+        
+        private void Update()
+        {
+            CalculatePosition();
+        }
+        
+        [Server]
+        void CalculatePosition()
+        {
+            List<Car> cars = CarUtility.Singleton.CarList();
+            positionClassement = CalculPosition(this, cars);
+        }
+
+        int CalculPosition(Car currentCar, List<Car> cars)
+        {
+            // TODO
+            return 0;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Checkpoint")
+            {
+                CmdEnterInNewCheckpoint();
+            }
+        }
+
+        [Command]
+        void CmdEnterInNewCheckpoint()
+        {
+            bool inRangeCheckpoint;
+
+            foreach (var checkpoint in GameObject.FindGameObjectsWithTag("Checkpoint"))
+            {
+                // float distanceToCheckpoint
+                // if()
+                
+                // Si je suis a coté d'un checkpoint et que ca correspond a l'index du checkpoint 
+                
+            }
+            
+            indexCheckPoint++;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/CarUtility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    // Pas rattaché à un joueur
+    public class CarUtility: MonoBehaviour
+    {
+        private List<Car> cars;
+
+        public static CarUtility Singleton;
+        
+        private void Awake()
+        {
+            Singleton = this;
+            cars = new List<Car>();
+        }
+
+        public List<Car> CarList()
+        {
+            return cars;
+        }
+
+        public void AddCar(Car car)
+        {
+            cars.Add(car);
+        }
+
+        private void Update()
+        {
+            foreach (Car car in cars)
+            {
+                
+            }
+        }
+    }
+}
