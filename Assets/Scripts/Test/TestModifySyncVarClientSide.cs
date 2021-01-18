c8626774ef99cb91a99b60358ff17a819ed36707
sachaamm <sachaamm@gmail.com>
2021-01-18 14:58:37 +0100

    syncVar Without authority

---
+++ Assets/Scripts/Test/TestModifySyncVarClientSide.cs
@@ -0,0 +1,36 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Test
+{
+    public class TestModifySyncVarClientSide : NetworkBehaviour
+    {
+        [SyncVar] private int life = 10;
+
+        public TextMesh textMesh;
+        void Update()
+        {
+            textMesh.text = life.ToString();
+        
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                life++;
+                transform.name = "Player with life " + life;
+                Toto();
+            }
+        }
+
+        [Command]
+        void Toto()
+        {
+            life *= 2;
+            Tata();
+        }
+
+        [ClientRpc]
+        void Tata()
+        {
+        
+        }
+    }
+}
